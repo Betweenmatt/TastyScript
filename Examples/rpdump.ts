873c61277c697eb7ed95cb468f4411c1ff040ccf
Matthew Andrews <32918527+TastyGod@users.noreply.github.com>
2018-02-24 20:38:28 -0500

    changes to fit with v1.2.1

--- Examples/rpdump.ts
+++ Examples/rpdump.ts
@@ -1,48 +1,53 @@
 @import Examples/tools.ts;
 @end
 override.Start(){
-	# set this loop to a large number, or the number of times the buy/grind loop
+	# set this loop to a large arbitrary number, or the number of times the buy/grind loop
 	# can be run before you run out of raid points (currentRaidPoints / (buyLoopIterations * 450))
 	Loop("RaidScreen").For(1000);
 }
 function.RaidScreen(i){
-	Print(DateTime);
-	Print(": Iteration number: ");
-	PrintLine(i);
+	PrintLine(DateTime).Concat(": Iteration number: ").Concat(i);
 	#touch the weapon
 	Touch(548, 463);
 	#on buy loop, do less iterations than space in inventory
-	Loop("BuyLoop").For(42);
+	#so if your inventory is 114/200, then use .For(85);
+	Loop("BuyLoop").For(85);
 	GoToInventoryAndGrind();
 }
 function.GoToInventoryAndGrind(){
+	#Some of these touches use longer sleep timers because
+	#sometimes screen lag would cause issues.
+	#if you have issues still, try increasing the sleep times,
+	#or adding sleep timers to the overloaded touches
+	
 	#touch back arrow twice
 	BackArrow();
 	BackArrow();
 	#touch inventory button
-	Touch(169,654,3000);
+	Touch(169,654,2000);
 	#touch grind
-	Touch(845,625,3000);
+	Touch(845,625);
 	#touch grind all
-	Touch(729,629,3000);
+	Touch(729,629);
 	#touch grind
-	Touch(631,594,3000);
+	Touch(631,594);
 	#touch yes;
-	Touch(631, 531,3000);
+	Touch(631, 531,3000);#sleep longer
 	#touch middle of screen
-	Touch(675,625,3000);
+	Touch(675,625);
 	BackArrow();
 	#touch forge
 	Touch(1195,638,3000);
 	#touch shop
-	Touch(632,563,3000);
+	Touch(632,563);
 }
 function.BackArrow(){
-	Touch(134,26,3000);
+	Touch(134,26, 2000);
 }
 function.BuyLoop(i){
 	#touch buy button
-	Touch(1008, 648);
+	Touch(1008, 648,300);
 	#confirm buy button
+	#removed with most recent patch
 	#Touch(631, 531);
 }
