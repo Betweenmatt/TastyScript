a517ae563c6a9392cd4f1cc960b0065f44dcfc9c
Matthew Andrews <32918527+TastyGod@users.noreply.github.com>
2018-03-06 13:37:16 -0500

    Update imagetest.ts

--- Examples/imagetest.ts
+++ Examples/imagetest.ts
@@ -9,18 +9,18 @@
 ###
 
 override.Start(){
-  #store the start time for the stats displayed when halted
+  	#store the start time for the stats displayed when halted
 	$var startTime = DateTime;
   
 	Loop("Check");
 }
 function.Check(var i){
 	#start the performance timer
-  Timer().Start();
+  	Timer().Start();
 	PrintLine(DateTime).Color("DarkYellow");
 	PrintLine("Iteration number: ").Concat(i).Color("Yellow");
 	#check the screen against "/yourimage.png" and print the result 
-  CheckScreen(
+  	CheckScreen(
 		=>(){
 			PrintLine("\n\t\t\tSucceed\n").Color("DarkGreen");
 		},
@@ -30,19 +30,19 @@ function.Check(var i){
 		"/yourimage.png");
 	#add one to the total iterations since the loop is zero-index
 	$var totalIterations = [i + 1];
-  #print the performance timer results for this iteration
+  	#print the performance timer results for this iteration
 	Print("Elapsed MS: ");
 	Timer().Print().Color("Magenta");
 	PrintLine("\n");
-  #stop the timer to prevent memory leaks!
-  #this probably isnt needed since .Start() is called at the beginning of the next loop
-  #and .Stop() is called at the halt function, but it doesnt hurt to make sure!
+  	#stop the timer to prevent memory leaks!
+  	#this probably isnt needed since .Start() is called at the beginning of the next loop
+  	#and .Stop() is called at the halt function, but it doesnt hurt to make sure!
 	Timer().Stop();
 }
 override.Halt(){
-  #stop the timer to prevent memory leaks!
+  	#stop the timer to prevent memory leaks!
 	Timer().Stop();
-  #end run stats
+  	#end run stats
 	PrintLine("Total iterations: ").Concat(totalIterations);
 	PrintLine("Start: ").Concat(startTime);
 	PrintLine("End: ").Concat(DateTime);
