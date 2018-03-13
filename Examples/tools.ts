e69b571304439d2777b2611b14a67292ebdddca4
Matthew Andrews <32918527+TastyGod@users.noreply.github.com>
2018-03-13 02:12:32 -0400

    added the Touch() override

--- Examples/tools.ts
+++ Examples/tools.ts
@@ -18,3 +18,28 @@ override.Halt(){
 function.Accept(){
 	Touch(631, 531);
 }
+###
+# This is a helper function which converts the x y coordinates
+# of a touch from my resolution(1280x720) to your resolution.
+# to use this helper all you have to do is assign global variables
+# with your resolution in the beginning of your script like so:
+# $var XRes = 1440;
+# $var YRes = 900;
+# Once those global variables are assigned, all Touch() functions
+# called in the script will be relative to the resolution you specified.
+###
+override.Touch(x,y,sleep){
+	var xcoord = x;
+	var ycoord = y;
+	if(XRes != null).And(YRes != null)
+		.then(=>(x,y,sleep){
+			var xcoord = [(x / 1280) * XRes];
+			var ycoord = [(y / 720) * YRes];
+		});
+	if(sleep != null)
+		.then(=>(sleep){
+			Base(xcoord,ycoord,sleep);
+		}).else(=>(){
+			Base(xcoord,ycoord);
+		});
+}
