1fd6a23186b59d75913fdcd620827cd54e6edec5
Tastygod <betweenmatt@gmail.com>
2018-04-05 20:25:54 -0400

    initial commit of gui library with test function

---
+++ TastyGUI/Functions/TestFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TastyScript;
+using TastyScript.Lang;
+using TastyScript.Lang.Functions;
+
+namespace TastyGUI.Functions
+{
+    [Function("TestFunction")]
+    public class TestFunction : FDefinition
+    {
+        public override string CallBase()
+        {
+            Main.IO.Print("Hello, World!");
+            return "";
+        }
+    }
+}
